0706f8b3a42029bd0ddfa079b18626b0f8576353
Thiago Ferreira <thiaago.dev@gmail.com>
2021-06-15 20:09:07 -0300

    Python3 POO2 - trabalhando com herança

---
+++ modelo.py
@@ -0,0 +1,48 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self._ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def ano(self):
+        return self._ano
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome.title()
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_likes()
+print(f'{vingadores.nome} - {vingadores.duracao} : {vingadores.likes}')
+
+atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_likes()
+atlanta.dar_likes()
+print(f'{atlanta.nome} - {atlanta.temporadas} : {atlanta.likes}')
